b37253192a31187f4cdfed16a7a9f6c0cc861a96
JakusDani <55551015+JakusDani@users.noreply.github.com>
2025-03-23 17:13:38 +0100

    #5 Add configuration to RecipeEntity

--- src/RecipeBook.Repository/Entities/RecipeEntity.cs
+++ src/RecipeBook.Repository/Entities/RecipeEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeBook.Repository.Entities;
+﻿namespace RecipeBook.Repository.Entities;
 
 public class RecipeEntity : BaseEntity
 {
@@ -14,6 +8,7 @@ public class RecipeEntity : BaseEntity
     public string MainImageLink { get; set; } = string.Empty;
     public string Instruction { get; set; } = string.Empty;
     public int PortionPerPerson { get; set; }
+    public int CategoryId  { get; set; }
     public CategoryEntity Category { get; set; } = null!;
     public ICollection<IngredientsEntity> Ingredients { get; set; } = [];
     public ICollection<ImagesEntity> Images { get; set; } = [];
--- src/RecipeBook.Repository/RecipeBookContext.cs
+++ src/RecipeBook.Repository/RecipeBookContext.cs
@@ -6,8 +6,16 @@ namespace RecipeBook.Repository;
 
 public class RecipeBookContext : DbContext
 {
-    public DbSet<RecipeEntity> Users { get; set; }
+    public DbSet<RecipeEntity> Recipe { get; set; }
+    public DbSet<CategoryEntity> Category { get; set; }
+    public DbSet<ImagesEntity> Image { get; set; }
+    public DbSet<IngredientsEntity> Ingredient { get; set; }
+    public DbSet<MeasurementSystemEntity> MeasurementSystem { get; set; }
+    public DbSet<UnitOfMeasurementEntity> UnitOfMeasurement { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlite("Data Source=Databases/RecipeBook.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RecipeBookContext).Assembly);
 }
